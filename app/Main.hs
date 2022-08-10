e2ca874cc282f310010771aaa2b97dfd3e77dab4
acidicMercury8 <acidicmercury8@outlook.com>
2022-08-10 13:57:06 +0300

    Add help messages for testing operators

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,12 @@ import Lib (runIncrParse, runSumParse)
 
 main :: IO ()
 main = do
+  putStrLn "Enter an expression like <integer_number_1>+<integer_number_2>"
   lineSum <- getLine
   case runSumParse lineSum of
     Right result -> putStrLn ("Result: " ++ show result)
     Left error -> print error
+  putStrLn "Enter an expression like <integer_number>++"
   lineIncr <- getLine
   case runIncrParse lineIncr of
     Right result -> putStrLn ("Result: " ++ show result)
