1e0fb674e195ff073d6383b52d308b8d46ed1fb7
acidicMercury8 <acidicmercury8@outlook.com>
2022-08-10 23:50:58 +0300

    Update parser module with helpers

--- src/ParserHelpers.hs
+++ src/ParserHelpers.hs
@@ -1,26 +1,9 @@
 module ParserHelpers where
 
-import Text.Parsec ( ParseError, many, parse, eof )
-import Text.Parsec.String ( Parser )
+import Text.Parsec (ParseError, parse)
 
-import qualified Text.Parsec.Token as Tok
-
-import Syntax ( Expr, AST )
-import Lexer ( lexer, reservedOp )
-import Parser ( function, expr )
-
-contents :: Parser a -> Parser a
-contents p = do
-  Tok.whiteSpace lexer
-  r <- p
-  eof
-  return r
-
-toplevel :: Parser [Expr]
-toplevel = many $ do
-    def <- function
-    reservedOp ";"
-    return def
+import Parser (expr, contents, toplevel)
+import Syntax (Expr, AST)
 
 parseExpr :: String -> Either ParseError Expr
 parseExpr = parse (contents expr) "<stdin>"
