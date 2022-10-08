10769e283bf38138206156d34cfe7883c4272b38
acidicMercury8 <acidicmercury8@outlook.com>
2022-10-08 14:35:13 +0300

    Add house price prediction and iris classifier projects

---
+++ mldotnet-samples/src/IrisClassifier/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.ML;
+
+using IrisClassifier;
+
+var mlContext = new MLContext();
+
+var dataLoader = mlContext.Data.CreateTextLoader<IrisData>(separatorChar: ',', hasHeader: true);
+var trainingData = dataLoader.Load(@"Datas\iris.data");
+
+var pipeline = mlContext.Transforms.Conversion.MapValueToKey("Label")
+    .Append(mlContext.Transforms
+        .Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"))
+    .Append(mlContext.MulticlassClassification.Trainers
+        .SdcaNonCalibrated(labelColumnName: "Label", featureColumnName: "Features"))
+    .Append(mlContext.Transforms.Conversion
+        .MapKeyToValue("PredictedLabel"));
+
+var model = pipeline.Fit(trainingData);
+
+var prediction = mlContext.Model
+    .CreatePredictionEngine<IrisData, IrisPrediction>(model)
+    .Predict(new IrisData() {
+        SepalLength = 3.3f,
+        SepalWidth = 1.6f,
+        PetalLength = 0.2f,
+        PetalWidth = 5.1f,
+    });
+
+Console.WriteLine($"Predicted flower type is: {prediction.PredictedLabels}");
+
+mlContext.Model.Save(model, trainingData.Schema, "irisTrainingSchema.zip");
