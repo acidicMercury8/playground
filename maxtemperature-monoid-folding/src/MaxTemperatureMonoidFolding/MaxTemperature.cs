4c4f518381f37d673d80d4de8862ad6d0ccde32a
acidicMercury8 <acidicmercury8@outlook.com>
2022-10-08 14:11:24 +0300

    Add monoid test project

---
+++ maxtemperature-monoid-folding/src/MaxTemperatureMonoidFolding/MaxTemperature.cs
@@ -0,0 +1,27 @@
+﻿namespace MaxTemperatureMonoidFolding;
+
+public class MaxTemperature : IMonoid<(int, int)> {
+    public (int, int) Zero() {
+        return (0, 0);
+    }
+
+    public bool IsZero((int, int) v) {
+        (int time, int value) = v;
+        return time.Equals(0) && value.Equals(0);
+    }
+
+    public (int, int) Op((int, int) a, (int, int) b) {
+        if (IsZero(a)) {
+            return b;
+        }
+        if (IsZero(b)) {
+            return a;
+        }
+
+        (var _, var aValue) = a;
+        (var _, var bValue) = b;
+        return aValue > bValue
+            ? a
+            : b;
+    }
+}
---
+++ maxtemperature-monoid-folding/src/MaxTemperatureMonoidFolding/Series.cs
@@ -0,0 +1,18 @@
+﻿namespace MaxTemperatureMonoidFolding;
+
+public class Series<T> {
+    private T[] Values { get; set; }
+
+    public Series(T[] values) {
+        Values = values;
+    }
+
+    public T Reduce(IMonoid<T> monoid) {
+        var result = monoid.Zero();
+        for (var i = 0; i < Values.Length; i++)
+        {
+            result = monoid.Op(result, Values[i]);
+        }
+        return result;
+    }
+}
