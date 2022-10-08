762d88e12b0d2f598e556dc77c980a31cbc8aed8
acidicMercury8 <acidicmercury8@outlook.com>
2022-10-08 17:41:20 +0300

    Add house price prediction and iris classifier projects

---
+++ mldotnet-samples/src/IrisClassifier/IrisData.fs
@@ -0,0 +1,16 @@
+﻿namespace IrisClassifier
+
+open Microsoft.ML.Data
+
+type IrisData = {
+    [<LoadColumn(0)>]
+    SepalLength: float32;
+    [<LoadColumn(1)>]
+    SepalWidth: float32;
+    [<LoadColumn(2)>]
+    PetalLength: float32;
+    [<LoadColumn(3)>]
+    PetalWidth: float32;
+    [<LoadColumn(4)>]
+    Label: string;
+}
---
+++ mldotnet-samples/src/IrisClassifier/Program.fs
@@ -0,0 +1,41 @@
+﻿open Microsoft.ML
+open Microsoft.ML.Data
+
+open IrisClassifier
+
+[<EntryPoint>]
+let main _ =
+    let mlContext = new MLContext()
+
+    let dataLoader = mlContext.Data.CreateTextLoader<IrisData>(separatorChar = ',', hasHeader = true)
+    let trainingData = dataLoader.Load(@"Datas\iris.data")
+
+    let pipeline =
+        EstimatorChain()
+            .Append(mlContext.Transforms.Conversion
+                .MapValueToKey("Label"))
+            .Append(mlContext.Transforms
+                .Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"))
+            .Append(mlContext.MulticlassClassification.Trainers
+                .SdcaNonCalibrated(labelColumnName = "Label", featureColumnName = "Features"))
+            .Append(mlContext.Transforms.Conversion
+                .MapKeyToValue("PredictedLabel"))
+
+    let model = pipeline.Fit trainingData
+
+    let prediction =
+        mlContext.Model
+            .CreatePredictionEngine<IrisData, IrisPrediction>(model)
+            .Predict({
+                SepalLength = 3.3f;
+                SepalWidth = 1.6f;
+                PetalLength = 0.2f;
+                PetalWidth = 5.1f;
+                Label = ""
+            })
+
+    printfn $"Predicted flower type is: {prediction.PredictedLabels}"
+
+    mlContext.Model.Save(model, trainingData.Schema, "irisSchema.zip")
+
+    0
