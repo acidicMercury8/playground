9f2b4dc34eca817be6ebba0bb884564afef086d1
acidicMercury8 <acidicmercury8@outlook.com>
2022-10-19 23:49:10 +0300

    Fix codestyle of standalone Vue project

--- aspdotnet-samples/src/standalonevueproject-backend/Program.cs
+++ aspdotnet-samples/src/standalonevueproject-backend/Program.cs
@@ -1,31 +1,28 @@
-namespace standalonevueproject_backend {
-    public class Program {
-        public static void Main(string[] args) {
-            var builder = WebApplication.CreateBuilder(args);
+namespace StandaloneVueProject.Backend;
 
-            // Add services to the container.
+public class Program {
+    public static void Main(string[] args) {
+        var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        // Add services to the container
 
-            var app = builder.Build();
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment()) {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+        var app = builder.Build();
 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+        // Configure the HTTP request pipeline
+        if (app.Environment.IsDevelopment()) {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
--- aspdotnet-samples/src/standalonevueproject-frontend/vue.config.js
+++ aspdotnet-samples/src/standalonevueproject-frontend/vue.config.js
@@ -1,16 +1,19 @@
 const fs = require('fs')
 const path = require('path')
 
-const baseFolder =
-    process.env.APPDATA !== undefined && process.env.APPDATA !== ''
-        ? `${process.env.APPDATA}/ASP.NET/https`
-        : `${process.env.HOME}/.aspnet/https`;
+const baseFolder = process.env.APPDATA !== undefined && process.env.APPDATA !== ''
+    ? `${process.env.APPDATA}/ASP.NET/https`
+    : `${process.env.HOME}/.aspnet/https`;
 
-const certificateArg = process.argv.map(arg => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0];
-const certificateName = certificateArg ? certificateArg.groups.value : "standalonevueproject-frontend";
+const certificateArg = process.argv
+    .map(arg => arg.match(/--name=(?<value>.+)/i))
+    .filter(Boolean)[0];
+const certificateName = certificateArg
+    ? certificateArg.groups.value
+    : "standalonevueproject-frontend";
 
 if (!certificateName) {
-    console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.')
+    console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly')
     process.exit(-1);
 }
 
