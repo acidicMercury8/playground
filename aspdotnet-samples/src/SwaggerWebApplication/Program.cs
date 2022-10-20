975f7d7c0c1069247e7eec48901dc7f675d1d481
acidicMercury8 <acidicmercury8@outlook.com>
2022-10-20 11:02:56 +0300

    Implement options in Swagger test project

--- aspdotnet-samples/src/SwaggerWebApplication/Program.cs
+++ aspdotnet-samples/src/SwaggerWebApplication/Program.cs
@@ -1,3 +1,7 @@
+using System.Reflection;
+
+using Microsoft.OpenApi.Models;
+
 namespace SwaggerWebApplication;
 
 public class Program {
@@ -7,17 +11,42 @@ public class Program {
         // Add services to the container
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(options => {
+            options.SwaggerDoc("v1", new OpenApiInfo {
+                Version = "v1",
+                Title = "ToDo API",
+                Description = "ASP.NET Core Web API for testing Swagger features",
+                TermsOfService = new Uri("https://localhost:5001/terms"),
+                Contact = new OpenApiContact {
+                    Name = "Example Contact",
+                    Url = new Uri("https://localhost:5001/contact")
+                },
+                License = new OpenApiLicense {
+                    Name = "Example License",
+                    Url = new Uri("https://localhost:5001/license")
+                },
+            });
+
+            var xmlFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFileName));
+        });
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline
         if (app.Environment.IsDevelopment()) {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwagger(/*options => {
+                options.SerializeAsV2 = true;
+            }*/);
+            app.UseSwaggerUI(options => {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+                //options.InjectStylesheet("/swagger-ui/custom.css");
+            });
         }
 
         app.UseHttpsRedirection();
+        //app.UseStaticFiles();
         app.UseAuthorization();
 
         app.MapControllers();
