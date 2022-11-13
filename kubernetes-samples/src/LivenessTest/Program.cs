af4bd20459acb44deb6c244419ba39e94b2300fe
acidicMercury8 <acidicmercury8@outlook.com>
2022-11-13 15:27:39 +0300

    Implement liveness test functionality

--- kubernetes-samples/src/LivenessTest/Program.cs
+++ kubernetes-samples/src/LivenessTest/Program.cs
@@ -1,31 +1,42 @@
-namespace LivenessTest {
-    public class Program {
-        public static void Main(string[] args) {
-            var builder = WebApplication.CreateBuilder(args);
+using LivenessTest.Services;
 
-            // Add services to the container.
+using Microsoft.AspNetCore.Hosting;
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+namespace LivenessTest;
 
-            var app = builder.Build();
+public class Program {
+    private static readonly int _port = 80;
+    private static readonly TimeSpan _timeout = TimeSpan.FromSeconds(1);
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment()) {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+    public static void Main(string[] args) {
+        var builder = WebApplication.CreateBuilder(args);
 
-            app.UseHttpsRedirection();
+        builder.WebHost.ConfigureKestrel(serverOptions => {
+            serverOptions.ListenAnyIP(_port);
+            serverOptions.Limits.KeepAliveTimeout = _timeout;
+        });
+        builder.WebHost.UseUrls();
 
-            app.UseAuthorization();
+        // Add services to the container
+        builder.Services.AddSingleton<LoremService>();
 
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-            app.MapControllers();
+        var app = builder.Build();
 
-            app.Run();
+        // Configure the HTTP request pipeline
+        if (app.Environment.IsDevelopment()) {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
