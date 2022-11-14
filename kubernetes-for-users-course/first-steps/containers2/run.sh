bf393d2154d545525eb17e643c8c351297b4e872
acidicMercury8 <acidicmercury8@outlook.com>
2022-11-14 23:12:26 +0300

    Update running script of containers exercise

--- kubernetes-for-users-course/first-steps/containers2/run.sh
+++ kubernetes-for-users-course/first-steps/containers2/run.sh
@@ -2,3 +2,7 @@ docker build . -t go:v0.2
 docker images
 docker run -d -p 8080:8080 go:v0.2
 docker ps
+
+docker login
+docker tag go:v0.2 acidicmercury8/goapp:v0.2
+docker push acidicmercury8/goapp:v0.2
