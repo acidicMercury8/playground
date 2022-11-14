2df110a041954d9a2826055e17dd73a75149be3c
acidicMercury8 <acidicmercury8@outlook.com>
2022-11-14 23:14:19 +0300

    Add files of first objects exercises

---
+++ kubernetes-for-users-course/first-steps/first-objects/apply-namespace.sh
@@ -0,0 +1,4 @@
+kubectl apply -f namespace.yaml
+kubectl get ns
+#kubectl create ns first-objects # Not recommended
+#kubectl delete -f namespace.yaml
---
+++ kubernetes-for-users-course/first-steps/first-objects/apply-pod.sh
@@ -0,0 +1,2 @@
+kubectl apply -f pod.yaml
+kubectl port-forward -n first-objects static-web 8080:8080
