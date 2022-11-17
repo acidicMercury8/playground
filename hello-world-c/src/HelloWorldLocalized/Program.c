6c162d51e4385e875bd5874b275764876e2036ba
acidicMercury8 <acidicmercury8@outlook.com>
2022-11-17 18:49:05 +0300

    Add localized hello world C console application

---
+++ hello-world-c/src/HelloWorldLocalized/Program.c
@@ -0,0 +1,13 @@
+﻿#include <locale.h>
+#include <conio.h>
+#include <stdio.h>
+
+int main(void) {
+    setlocale(LC_ALL, "Rus");
+
+    printf("Привет, мир!");
+
+    printf("\nДля продолжения нажмите любую клавишу . . . ");
+    char result = _getch();
+    return 0;
+}
