5ec702405554aa5aa2ef042af4d87071ad2cec06
acidicMercury8 <acidicmercury8@outlook.com>
2022-12-16 00:32:11 +0300

    Refactoring using `CSharpSyntaxWalker`

--- static-analysis-tools-csharp/src/CoincideIfStatementBranchesAnalyzer/Program.cs
+++ static-analysis-tools-csharp/src/CoincideIfStatementBranchesAnalyzer/Program.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -9,6 +10,35 @@ using Microsoft.CodeAnalysis.MSBuild;
 
 namespace CoincideIfStatementBranchesAnalyzer {
     public class Program {
+        public class IfWalker : CSharpSyntaxWalker {
+            public StringBuilder Warnings { get; } = new StringBuilder();
+
+            const string WarningMessageFormat =
+                "'if' with equal 'then' and 'else' blocks is found in file {0} at line {1}";
+
+            public static bool ApplyRule(IfStatementSyntax ifStatement) {
+                if (ifStatement?.Else == null) {
+                    return false;
+                }
+
+                var thenBody = ifStatement.Statement;
+                var elseBody = ifStatement.Else.Statement;
+                return SyntaxFactory.AreEquivalent(thenBody, elseBody);
+            }
+
+            public override void VisitIfStatement(IfStatementSyntax node) {
+                if (ApplyRule(node)) {
+                    var lineNumber = node.GetLocation()
+                        .GetLineSpan()
+                        .StartLinePosition.Line + 1;
+                    Warnings.AppendLine(string.Format(WarningMessageFormat,
+                        node.SyntaxTree.FilePath, lineNumber));
+                }
+
+                base.VisitIfStatement(node);
+            }
+        }
+
         private static Project GetProjectFromSolution(string solutionPath,
             MSBuildWorkspace workspace) {
             MSBuildLocator.RegisterDefaults();
@@ -16,14 +46,9 @@ namespace CoincideIfStatementBranchesAnalyzer {
             return currentSolution.Projects.Single();
         }
 
-        private static bool ApplyRule(IfStatementSyntax ifStatement) {
-            if (ifStatement?.Else == null) {
-                return false;
-            }
-
-            var thenBody = ifStatement.Statement;
-            var elseBody = ifStatement.Else.Statement;
-            return SyntaxFactory.AreEquivalent(thenBody, elseBody);
+        public static void StartWalker(IfWalker ifWalker, SyntaxNode syntaxNode) {
+            ifWalker.Warnings.Clear();
+            ifWalker.Visit(syntaxNode);
         }
 
         public static void Main(string[] args) {
@@ -34,27 +59,18 @@ namespace CoincideIfStatementBranchesAnalyzer {
             var solutionPath = args[0];
             var logPath = args[1];
 
-            var warnings = new StringBuilder();
-            const string warningMessageFormat =
-                "'if' with equal 'then' and 'else' blocks is found in file {0} at line {1}";
-
             MSBuildLocator.RegisterDefaults();
 
             using (var workspace = MSBuildWorkspace.Create()) {
                 var currentProject = GetProjectFromSolution(solutionPath, workspace);
                 foreach (var document in currentProject.Documents) {
                     var tree = document.GetSyntaxTreeAsync().Result;
-                    var ifStatementNodes = tree.GetRoot()
-                        .DescendantNodesAndSelf()
-                        .OfType<IfStatementSyntax>();
-                    foreach (var ifStatement in ifStatementNodes) {
-                        if (ApplyRule(ifStatement)) {
-                            var lineNumber = ifStatement.GetLocation()
-                                .GetLineSpan()
-                                .StartLinePosition.Line + 1;
-                            warnings.AppendLine(string.Format(warningMessageFormat,
-                                document.FilePath, lineNumber));
-                        }
+                    var ifWalker = new IfWalker();
+                    StartWalker(ifWalker, tree.GetRoot());
+
+                    var warnings = ifWalker.Warnings;
+                    if (warnings.Length != 0) {
+                        File.AppendAllText(logPath, warnings.ToString());
                     }
                 }
             }
