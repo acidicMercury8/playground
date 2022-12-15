96f657b2f18efd34265908c989ac141fc37e56fa
acidicMercury8 <acidicmercury8@outlook.com>
2022-12-15 12:04:12 +0300

    Add empty code analyzer projects

---
+++ static-analysis-tools-csharp/src/SimpleCodeFixAnalyzer/SimpleCodeFixAnalyzer.Test/SimpleCodeFixAnalyzerUnitTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using VerifyCS = SimpleCodeFixAnalyzer.Test.CSharpCodeFixVerifier<
+    SimpleCodeFixAnalyzer.SimpleCodeFixAnalyzerAnalyzer,
+    SimpleCodeFixAnalyzer.SimpleCodeFixAnalyzerCodeFixProvider>;
+
+namespace SimpleCodeFixAnalyzer.Test {
+    [TestClass]
+    public class SimpleCodeFixAnalyzerUnitTest {
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task TestMethod1() {
+            var test = @"";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public async Task TestMethod2() {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class {|#0:TypeName|}
+        {   
+        }
+    }";
+
+            var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TYPENAME
+        {   
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic("SimpleCodeFixAnalyzer").WithLocation(0).WithArguments("TypeName");
+            await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
+        }
+    }
+}
