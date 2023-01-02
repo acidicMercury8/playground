1c355f99238e4bc07a4cfd3d3c00489b1d62a355
acidicMercury8 <acidicmercury8@outlook.com>
2023-01-03 00:01:27 +0300

    Rewrite scene of JavaFX hello world application

--- javafx-samples/hello-world/src/main/java/com/example/helloworld/HelloApplication.java
+++ javafx-samples/hello-world/src/main/java/com/example/helloworld/HelloApplication.java
@@ -2,52 +2,31 @@ package com.example.helloworld;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 import javafx.scene.Group;
-import javafx.scene.text.Text;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
     public static void main(String[] args) {
-        System.out.println("Application: main");
-
         launch();
     }
 
     @Override
-    public void init() throws Exception {
-        System.out.println("Application: init");
-        super.init();
-    }
-
-    @Override
     public void start(Stage stage) {
-        System.out.println("Application: start");
+        var label = new Label("Hello");
+        var button = new Button("Button");
+        var group = new Group(button);
 
-        var unnamedParameters = getParameters().getUnnamed();
-        var i = 0;
-
-        for (var parameter: unnamedParameters) {
-            i++;
-            System.out.printf("%d - %s \n", i, parameter);
-        }
-
-        var text = new Text("Hello, world!");
-        text.setLayoutX(80);
-        text.setLayoutY(80);
-
-        var group = new Group(text);
-        var scene = new Scene(group);
+        var root = new FlowPane(label, group);
+        var scene = new Scene(root);
         stage.setScene(scene);
+
         stage.setTitle("JavaFX Application");
         stage.setWidth(300);
-        stage.setHeight(250);
+        stage.setHeight(150);
 
         stage.show();
-    }
-
-    @Override
-    public void stop() throws Exception {
-        System.out.println("Application: stop");
-        super.stop();
     }
 }
