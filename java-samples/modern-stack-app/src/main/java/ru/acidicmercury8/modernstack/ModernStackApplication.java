8b008413e84c4d2866f3d4657a3f0d3aac905edd
acidicMercury8 <acidicmercury8@outlook.com>
2023-05-01 15:32:30 +0300

    Add empty Java Spring project

---
+++ java-samples/modern-stack-app/src/main/java/ru/acidicmercury8/modernstack/ModernStackApplication.java
@@ -0,0 +1,11 @@
+package ru.acidicmercury8.modernstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernStackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ModernStackApplication.class, args);
+    }
+}
---
+++ java-samples/modern-stack-app/src/test/java/ru/acidicmercury8/modernstack/ModernStackApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.acidicmercury8.modernstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernStackApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
