0197dea263771442cb61b0e30d521106ce23ef37
acidicMercury8 <acidicmercury8@outlook.com>
2024-01-08 20:36:57 +0300

    Implement OpenGL initialization

--- opengl-project/src/OpenGLApp/Program.cpp
+++ opengl-project/src/OpenGLApp/Program.cpp
@@ -1,4 +1,55 @@
-#include <GLFW\glfw3.h>
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+    std::cout << key << std::endl;
+
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
 
 int main() {
+    std::cout << "Initializing GLFW context with OpenGL 3.3" << std::endl;
+
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGLApp", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, KeyCallback);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Failed to initialize GLEW!" << std::endl;
+        return -1;
+    }
+
+    int width;
+    int height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
 }
