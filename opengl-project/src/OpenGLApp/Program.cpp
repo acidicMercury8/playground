a3f256fe0dcd3a8d374aaa9e28ece17a780b4121
acidicMercury8 <acidicmercury8@outlook.com>
2024-01-08 22:06:44 +0300

    Implement shader parameters passing in OpenGL

--- opengl-project/src/OpenGLApp/Program.cpp
+++ opengl-project/src/OpenGLApp/Program.cpp
@@ -8,16 +8,21 @@ const GLchar* vertexShaderSource =
     "\n"
     "layout (location = 0) in vec3 position;\n"
     "\n"
+    "out vec4 vertexColor;"
+    "\n"
     "void main() {\n"
-    "    gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+    "    gl_Position = vec4(position, 1.0);\n"
+    "    vertexColor = vec4(0.5f, 0.0f, 0.0f, 1.0f);\n"
     "}\n\0";
 const GLchar* fragmentShaderSource =
     "#version 330 core\n"
     "\n"
+    "in vec4 vertexColor;"
+    "\n"
     "out vec4 color;\n"
     "\n"
     "void main() {\n"
-    "    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "    color = vertexColor;\n"
     "}\n\0";
 
 void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
