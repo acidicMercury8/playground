e69ac3131b0f1eb228bd933424dce31de1959e45
acidicMercury8 <acidicmercury8@outlook.com>
2025-06-15 14:28:19 +0300

    Add primitive asm loader

---
+++ loader.asm
@@ -0,0 +1,37 @@
+﻿start:
+    jmp .init
+
+.entrypoint:
+    mov si, text_string
+    call print_string
+
+    jmp $
+
+.init:
+    mov ax, 07C0h
+    add ax, 288
+    mov ss, ax
+    mov sp, 4096
+    mov ax, 07C0h
+    mov ds, ax
+
+    jmp .entrypoint
+
+print_string:
+    mov ah, 0Eh
+
+.repeat:
+    lodsb
+
+    cmp al, 0
+    je .done
+
+    int 10h
+    jmp .repeat
+
+.done:
+    ret
+
+text_string      db 'TextString', 0
+times 510-($-$$) db 0
+                 dw 0xAA55
