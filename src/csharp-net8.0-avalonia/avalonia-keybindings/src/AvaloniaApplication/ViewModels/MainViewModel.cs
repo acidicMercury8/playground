75d5809c3832c116398c30adba156787ff6ebd79
acidicMercury8 <acidicmercury8@outlook.com>
2025-11-10 23:00:48 +0300

    Add .NET 8 Avalonia key bindings sample

---
+++ src/csharp-net8.0-avalonia/avalonia-keybindings/src/AvaloniaApplication/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace AvaloniaApplication.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _greeting = "Welcome to Avalonia!";
+
+    [ObservableProperty]
+    private string _inputText = "";
+
+    [RelayCommand]
+    private void AltD(string input)
+    {
+        Greeting = input;
+    }
+}
